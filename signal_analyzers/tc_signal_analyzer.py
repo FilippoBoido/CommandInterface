4800319e5b3a861634cc5383c44ab47bf37d0664
Boido <filippo.boido@factoryautomation.info>
2023-07-22 20:24:47 +0200

    feat: add ignore ads symbols logic

--- signal_analyzers/tc_signal_analyzer.py
+++ signal_analyzers/tc_signal_analyzer.py
@@ -1,4 +1,5 @@
 import dataclasses
+import os
 from dataclasses import dataclass
 
 from prompt_toolkit import print_formatted_text, HTML
@@ -23,7 +24,6 @@ class Symbol:
     value: None
 
 
-# TODO: Add list of symbols to ignore
 class TCSignalAnalyzer(SignalAnalyzer):
 
     def __init__(self, ams_net_id='127.0.0.1.1.1'):
@@ -31,17 +31,27 @@ class TCSignalAnalyzer(SignalAnalyzer):
         self._plc = pyads.Connection(ams_net_id, pyads.PORT_TC3PLC1)
         self._plc.open()
         self._symbols = []
+        self.ignore_list_path = 'ignore_ada_symbols.txt'
 
     async def eval(self, signal: Signal):
         tc_signal = TCSignal(**dataclasses.asdict(signal))
         try:
             if tc_signal.all_symbols:
+                # Get a list of symbols to ignore
+                if os.path.isfile(self.ignore_list_path):
+                    with open(self.ignore_list_path, 'r') as ignore_symbols_file:
+                        ignore_symbols = ignore_symbols_file.read().split('\n')
+
                 symbols = self._plc.get_all_symbols()
+                filtered_symbols = []
                 for symbol in symbols:
+                    if symbol.name in ignore_symbols:
+                        continue
+                    filtered_symbols.append(symbol)
                     if symbol.plc_type:
                         symbol.read()
 
-                self._symbols = payload_to_dataclass(symbols, Symbol)
+                self._symbols = payload_to_dataclass(filtered_symbols, Symbol)
 
                 table = fill_table(self._symbols, Symbol)
                 print(table)
