64d5168ccf1e0bd895c4a47aaf764a4456920e4c
Boido <filippo.boido@factoryautomation.info>
2023-07-22 15:36:54 +0200

    refactor: use NestedCompleter instead of WordCompleter

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import asyncio
+from typing import Optional
 
 from prompt_toolkit import PromptSession
-from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.completion import NestedCompleter
 
 from signal_analyzers.fio_signal_analyzer import FIOSignalAnalyzer
 from signal_analyzers.generic_signal_analyzers import SignalAnalyzer
+from signal_analyzers.tc_signal_analyzer import TCSignalAnalyzer
 from signals.fio_signals import FIOSignalDict
 from signals.generic_signals import SignalDict, Signal
+from signals.tc_signals import TCSignalDict
 
 
 async def input_controller(queue, signal_dict: SignalDict):
     while True:
-        completer = WordCompleter(list(signal_dict.keys()))
+        completer = NestedCompleter.from_nested_dict(dict([(key, None) for key in signal_dict.keys()]))
+
         session = PromptSession(completer=completer, multiline=False)
-        user_input = await session.prompt_async()
-        if user_input in signal_dict:
-            signal = signal_dict[user_input]
-            queue.put_nowait(signal)
-            if signal.stop:
+        user_input: str = await session.prompt_async()
+        signal: Optional[Signal] = None
+        for key, value in signal_dict.items():
+            if key in user_input:
+                signal: Signal = value
+                if key != user_input:
+                    signal.payload = user_input.split(key)[1].strip()
+                queue.put_nowait(signal)
                 break
+        if signal and signal.stop:
+            break
 
 
 async def app_loop(queue, signal_analyzer: SignalAnalyzer):
@@ -34,10 +43,11 @@ async def app_loop(queue, signal_analyzer: SignalAnalyzer):
 
 async def main():
     queue = asyncio.Queue()
-    signal_dict = FIOSignalDict()
-    signal_analyzer = FIOSignalAnalyzer()
-
-    await asyncio.gather(input_controller(queue, signal_dict), app_loop(queue, signal_analyzer))
+    # fio_signal_dict = FIOSignalDict()
+    # fio_signal_analyzer = FIOSignalAnalyzer()
+    tc_signal_dict = TCSignalDict()
+    tc_signal_analyzer = TCSignalAnalyzer()
+    await asyncio.gather(input_controller(queue, tc_signal_dict), app_loop(queue, tc_signal_analyzer))
 
 
 if __name__ == '__main__':
