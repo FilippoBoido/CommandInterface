92417566439316093e01458d535c5ea677678b36
Boido <filippo.boido@factoryautomation.info>
2023-08-18 16:36:02 +0200

    fix: add include modules path to pyinstaller task

--- tasks.py
+++ tasks.py
@@ -3,6 +3,6 @@ from invoke import task, run
 
 @task
 def build_exe(c):
-    cmd = "pyinstaller --name tcexplorer --onefile implementations/tc/main.py"
+    cmd = "pyinstaller -p implementations/tc --name tcexplorer --onefile implementations/tc/main.py"
     result = run(cmd, hide=True, warn=True)
     print(result)
